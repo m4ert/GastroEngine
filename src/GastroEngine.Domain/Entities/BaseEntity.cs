e3de4912f1e621586276db5a1a93bee7871f464b
Angel Marte <angel.marte@airplane.solutions>
2026-07-05 14:52:02 -0400

    Add createdBy and ModifiedBy

--- src/GastroEngine.Domain/Entities/BaseEntity.cs
+++ src/GastroEngine.Domain/Entities/BaseEntity.cs
@@ -3,6 +3,10 @@ namespace GastroEngine.Domain.Entities;
 public abstract record BaseEntity
 {
     public required Guid Id { get; init; }
+    
+    public required Guid CreatedByUser { get;  init; }
+    public required Guid ModifiedByUser { get;  init; }
+    
     public required DateTimeOffset CreationDate { get; init; } =  DateTimeOffset.Now;
     public required DateTimeOffset ModificationDate { get; init; } =  DateTimeOffset.Now;
 }
