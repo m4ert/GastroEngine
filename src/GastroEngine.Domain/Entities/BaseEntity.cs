d3756a83d89d2dbc4774f8921bffbeffdbd4c0aa
Angel Marte <m4ert@proton.me>
2026-07-08 16:51:20 -0400

    Replace `Guid` for `UInt64`

--- src/GastroEngine.Domain/Entities/BaseEntity.cs
+++ src/GastroEngine.Domain/Entities/BaseEntity.cs
@@ -2,10 +2,10 @@ namespace GastroEngine.Domain.Entities;
 
 public abstract record BaseEntity
 {
-    public required Guid Id { get; init; }
+    public required UInt64 Id { get; init; }
     
-    public required Guid CreatedByUser { get;  init; }
-    public required Guid ModifiedByUser { get;  init; }
+    public required UInt64 CreatedByUser { get;  init; }
+    public required UInt64 ModifiedByUser { get;  init; }
     
     public required DateTimeOffset CreationDate { get; init; } =  DateTimeOffset.Now;
     public required DateTimeOffset ModificationDate { get; init; } =  DateTimeOffset.Now;
