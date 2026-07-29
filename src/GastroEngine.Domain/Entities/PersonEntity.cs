289d4bfe05a16cb69dedb368bd9653f8f8abb3b9
Angel Marte <m4ert@proton.me>
2026-07-28 21:58:48 -0400

    Move Email and Phone Number to PersonEntity

--- src/GastroEngine.Domain/Entities/PersonEntity.cs
+++ src/GastroEngine.Domain/Entities/PersonEntity.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GastroEngine.Domain.Entities;
 
 public abstract record PersonEntity : BaseEntity
 {
     public required string Name { get; init; }
     public required string LastName { get; init; }
+    
+    [Phone]
+    public string? PhoneNumber { get; init; }
+    
+    [EmailAddress]
+    public string? EmailAddress { get; init; }
 }
