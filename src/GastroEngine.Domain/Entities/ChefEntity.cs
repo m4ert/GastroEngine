03c7672b36576f8351243cdcc82822cb44b1e6e8
Angel Marte <m4ert@proton.me>
2026-07-31 17:33:20 -0400

    Remove Data Annotations

--- src/GastroEngine.Domain/Entities/ChefEntity.cs
+++ src/GastroEngine.Domain/Entities/ChefEntity.cs
@@ -1,10 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace GastroEngine.Domain.Entities;
 
 public record ChefEntity : PersonEntity
 {
-    
-    [Range(1, 100)]
     public required ushort ExperienceLevel { get; init; }
 }
