c47996452f4bbc37567f1d0ea5032ebeadd1687d
Angel Marte <m4ert@proton.me>
2026-08-04 23:54:29 -0400

    Add Product Preparation

--- src/GastroEngine.Domain/Entities/ProductPreparationEntity.cs
+++ src/GastroEngine.Domain/Entities/ProductPreparationEntity.cs
@@ -1,3 +1,5 @@
+using GastroEngine.Domain.Enums;
+
 namespace GastroEngine.Domain.Entities;
 
 public record ProductPreparationEntity : BaseEntity
@@ -5,5 +7,7 @@ public record ProductPreparationEntity : BaseEntity
     public required Int32 Index { get; init; }
     public required UInt64 ProductId { get; init; }
     public required string Name { get; init; }
+    public required UnitOfMeasure Unit { get; init; }
+    public required decimal TotalUnits { get; init; }
     public required decimal DurationInSeconds { get; init; }
 }
---
+++ src/GastroEngine.Domain/Enums/UnitOfMeasure.cs
@@ -0,0 +1,15 @@
+namespace GastroEngine.Domain.Enums;
+
+public enum UnitOfMeasure
+{
+    None,
+    
+    Pound,
+    Kilogram,
+    Milligram,
+    
+    Milliliter,
+    Liter,
+    FluidOunce,
+    Gallon
+}
